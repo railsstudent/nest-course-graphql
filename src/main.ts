ff746c926544994caf5ad172c0a9f37d130a05ba
Connie Leung <cleung334@gmail.com>
2021-01-11 22:01:18 +0800

    express middleware
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
+import * as rateLimit from 'express-rate-limit';
+import * as compression from 'compression';
+import * as bodyParser from 'body-parser';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.use(compression());
+  app.use(helmet());
+  app.use(bodyParser.json())
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(morgan('dev'));
+
+  app.use(
+    rateLimit({
+      windowMs: 1 * 60 * 1000, // 15 minutes
+      max: 100, // limit each IP to 500 requests per windowMs
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
