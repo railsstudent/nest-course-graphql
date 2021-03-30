ff48898ffaec72a02e9b7c16a75606a0412d958e
Connie Leung <cleung334@gmail.com>
2021-03-31 07:33:48 +0800

    fetch: next lessons
    Signed-off-by: Connie Leung <cleung334@gmail.com>

--- src/course/resolvers/paginated-items.resolver.ts
+++ src/course/resolvers/paginated-items.resolver.ts
@@ -1,14 +1,22 @@
-import { Args, Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
 import { CursorPaginationArgs } from '../dto'
 import { PaginatedItems } from '../entities'
-import { CourseService } from '../services'
+import { CourseService, LessonService } from '../services'
 
 @Resolver(() => PaginatedItems)
 export class PaginatedItemsResolver {
-  constructor(private courseService: CourseService) {}
+  constructor(private courseService: CourseService, private lessonService: LessonService) {}
 
   @Query(() => PaginatedItems, { nullable: true })
   async courses(@Args('args') args: CursorPaginationArgs): Promise<PaginatedItems> {
     return await this.courseService.getCourses(args)
+  }
+
+  @Mutation(() => PaginatedItems)
+  async nextLessons(
+    @Args('courseId') courseId: string,
+    @Args('args') args: CursorPaginationArgs,
+  ): Promise<PaginatedItems> {
+    return await this.lessonService.getPaginatedLessons({ ...args, courseId })
   }
 }
